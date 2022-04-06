dfbc2c27d26a7a9e2410b4ab2ebace6412d2e615
NathanDV <76598451+NathanDV0@users.noreply.github.com>
2022-04-06 12:31:57 -0400

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  	$(function () {
+    		$(".loader-wrapper").fadeOut(2000, function () {
+    			  $(".content").fadeIn(1000);
+            $(".menu-wrap").fadeIn(1000);
+    		});
+  	});
+});
