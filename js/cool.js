453e71fbc02206b054a4857659cc3a3100b80b70
NathanDV <76598451+NathanDVS@users.noreply.github.com>
2022-04-12 14:13:32 -0400

    Delete cool.js

--- js/cool.js
+++
@@ -1,9 +0,0 @@
-const button = document.createElement('button');
-let count = 0;
-
-button.onclick = () => {
-  count += 1;
-  button.value = "Count: " + count;
-}
-
-document.body.appendChild(button);
